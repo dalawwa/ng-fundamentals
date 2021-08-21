6efe9097c0aba33890fbf459fc60b56cd588a78a
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-21 21:25:05 +0200

    create basic events-list component

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  templateUrl: './events-list.component.html',
+})
+export class EventsListComponent {
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England',
+    },
+  };
+}
