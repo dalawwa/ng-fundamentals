c973ee8f294c6d39087d3698b5a109401c7e7434
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-22 19:18:35 +0200

    use ActivatedRoute to get the url parameter :id (cast it to Number as it is a string)

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -17,9 +18,14 @@ import { EventService } from '../shared/event.service';
 })
 export class EventDetailsComponent implements OnInit {
   event: any;
-  constructor(private eventService: EventService) {}
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(
+      Number(this.route.snapshot.params['id'])
+    );
   }
 }
