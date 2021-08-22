3551a6d78c5b372aed4b0090de49e1e0d3c9e1c3
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-22 19:38:05 +0200

    add cancel button click handler in create event to redirect to /events

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { EventService } from './shared/event.service';
 
 @Component({
   templateUrl: './create-event.component.html',
 })
 export class CreateEventComponent {
-  constructor(private eventService: EventService) {}
+  constructor(private router: Router, private eventService: EventService) {}
+
+  cancel() {
+    this.router.navigateByUrl('/events');
+    // same as this.router.navigate(['/events'])
+  }
 }
