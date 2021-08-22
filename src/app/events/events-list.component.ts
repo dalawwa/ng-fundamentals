7964b103cd5f71d788cff66b430c4d4bbedfcac2
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-22 12:04:28 +0200

    add safe navigation operator and support online events

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -91,11 +91,7 @@ export class EventsListComponent {
       time: '9:00 am',
       price: 950.0,
       imageUrl: '/assets/images/ng-nl.png',
-      location: {
-        address: 'The NG-NL Convention Center & Scuba Shop',
-        city: 'Amsterdam',
-        country: 'Netherlands',
-      },
+      onlineUrl: 'http://ng-nl.org/',
       sessions: [
         {
           id: 1,
