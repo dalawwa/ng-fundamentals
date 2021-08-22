c1af51be85d050dcdd93032a6327ced3f80b3f90
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-22 11:27:39 +0200

    add nav component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
+    NavBarComponent,
   ],
   imports: [BrowserModule],
   providers: [],
