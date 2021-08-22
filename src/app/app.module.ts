bd94feea6d32432119b84ba66bb03effbfa32b8a
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-22 19:04:00 +0200

    add RouterModule in app-module imports to load our routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ToastrService } from './common/toastr.service';
+import { RouterModule } from '@angular/router';
 
+import { ToastrService } from './common/toastr.service';
 import { EventsAppComponent } from './events-app.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
 
 @NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
@@ -17,7 +20,6 @@ import { NavBarComponent } from './nav/navbar.component';
     NavBarComponent,
     EventDetailsComponent,
   ],
-  imports: [BrowserModule],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent],
 })
