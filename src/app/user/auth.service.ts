2e748a3044629e8abaa2823f31a075b20c263af3
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-23 21:50:26 +0200

    create interface IUser and auth service

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable()
+export class AuthService {
+  currentUser?: IUser;
+
+  loginUser(userName: string, password: string): void {
+    // temporary
+    this.currentUser = {
+      id: 1,
+      userName,
+      firstName: 'John',
+      lastName: 'Papa',
+    };
+  }
+
+  isAuthenticated(): boolean {
+    return Boolean(this.currentUser);
+  }
+}
