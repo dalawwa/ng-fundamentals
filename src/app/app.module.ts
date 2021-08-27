0343ce32521a8efc7e31e09580f3da3d71a4f55f
Dany Alawwa <dany.alawwa+git@gmail.com>
2021-08-27 12:18:07 +0200

    add create session component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,22 @@ import {
   EventsListComponent,
   EventsThumbnailComponent,
   EventService,
+  SessionService,
 } from './events';
 import { NavBarComponent } from './nav';
 import { appRoutes } from './routes';
 import { AuthService } from './user/auth.service';
 import { FormValidatorService } from './common/form-validator.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreateSessionComponent } from './events/event-details/create-session.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
@@ -30,21 +37,26 @@ import { FormsModule } from '@angular/forms';
     EventDetailsComponent,
     CreateEventComponent,
     Error404Component,
+    CreateSessionComponent,
   ],
   providers: [
     EventService,
     ToastrService,
     EventRouteActivatorService,
     { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }, // when not making a full service
+    { provide: 'canDeactivateCreateSession', useValue: checkDirtyState }, // when not making a full service
     EventListResolver,
     AuthService,
     FormValidatorService,
+    SessionService,
   ],
   bootstrap: [EventsAppComponent],
 })
 export class AppModule {}
 
-export function checkDirtyState(component: CreateEventComponent) {
+export function checkDirtyState(
+  component: CreateEventComponent | CreateSessionComponent
+) {
   if (component.isDirty) {
     return window.confirm(
       'You have not saved this event, do you really want to cancel ?'
